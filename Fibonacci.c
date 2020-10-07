04df3d6a0167febd1a54ac8742418889ddd21d87
Tanishka Gupta <gtanishka8@gmail.com>
2020-10-07 18:58:49 +0530

    changes
    please accept the following changes

--- Fibonacci.c
+++ Fibonacci.c
@@ -1,23 +1,64 @@
 #include <stdio.h>
-#include <stdlib.h>
-void fibonacci(int num)
+//fib(0)=0 and fib(1)=1
+//using recursion
+int fib1(int n)
 {
-	if(num == 1 || num == 2)printf("1\n");
-	else
-	{
-		int *fib = (int *)malloc(num*sizeof(int));
-		fib[0] = 1;
-		fib[1] = 1;
-		for(int i = 2 ; i < num ; i++)
-			fib[i] = fib[i-1] + fib[i-2];
-		for(int i = 0; i < num ; i++)
-			printf("%d\n" , fib[i]);
-	}
+    if (n == 0)
+    {
+        return 0;
+    }
+    else if (n == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return fib1(n - 1) + fib1(n - 2);
+    }
 }
-void main()
+//using dynamic programming
+int fib2(int n)
 {
-	printf("Enter Number of Elements : ");
-	int num;
-	scanf("%d" , &num);
-	fibonacci(num);
+    int arr[n + 1];
+    arr[0] = 0;
+    arr[1] = 1;
+    for (int i = 2; i <= n; i++)
+    {
+        arr[i] = arr[i - 1] + arr[i - 2];
+    }
+    return arr[n];
+}
+//dynamic, space optimised
+int fib3(int n)
+{
+    int a, b, c;
+    a = 0;
+    b = 1;
+    for (int i = 2; i <= n; i++)
+    {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    if (n == 0)
+    {
+        return 0;
+    }
+    else if (n == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return c;
+    }
+}
+int main()
+{
+    int N;
+    scanf("%d", &N);
+    int res;
+    res = fib3(N);
+    printf("%d\n", res);
+    return 0;
 }
--- heapSort.cpp
+++ heapSort.cpp
@@ -1,55 +1,58 @@
-/*  
-    Consider an array 'arr' which is to be sorted using Heap Sort.
-
-    a) Initially build a max heap of elements in 'arr'.
-    b) The root element, that is arr[0], will contain maximum 
-       element of arr. After that, swap this element with the 
-       last element of arr and heapify the max heap excluding the 
-       last element which is already in its correct position and 
-       then decrease the length of heap by one.
-    c) Repeat the step 2, until all the elements are in their correct position.
-
-    Time complexity: O(n + n*logn) = O(n*logn)
-*/
-
-#include<bits/stdc++.h>
-using namespace std;
-
-// Auxiliary Functions
-void printArray(int arr[], int n) { 
+using namespace std; 
+  
+// To heapify a subtree rooted with node i which is 
+// an index in arr[]. n is size of heap 
+void heapify(int arr[], int n, int i) 
+{ 
+    int largest = i; // Initialize largest as root 
+    int l = 2*i + 1; // left = 2*i + 1 
+    int r = 2*i + 2; // right = 2*i + 2 
+  
+    // If left child is larger than root 
+    if (l < n && arr[l] > arr[largest]) 
+        largest = l; 
+  
+    // If right child is larger than largest so far 
+    if (r < n && arr[r] > arr[largest]) 
+        largest = r; 
+  
+    // If largest is not root 
+    if (largest != i) 
+    { 
+        swap(arr[i], arr[largest]); 
+  
+        // Recursively heapify the affected sub-tree 
+        heapify(arr, n, largest); 
+    } 
+} 
+  
+// main function to do heap sort 
+void heapSort(int arr[], int n) 
+{ 
+    // Build heap (rearrange array) 
+    for (int i = n / 2 - 1; i >= 0; i--) 
+        heapify(arr, n, i); 
+  
+    // One by one extract an element from heap 
+    for (int i=n-1; i>0; i--) 
+    { 
+        // Move current root to end 
+        swap(arr[0], arr[i]); 
+  
+        // call max heapify on the reduced heap 
+        heapify(arr, i, 0); 
+    } 
+} 
+  
+/* A utility function to print array of size n */
+void printArray(int arr[], int n) 
+{ 
     for (int i=0; i<n; ++i) 
         cout << arr[i] << " "; 
     cout << "\n"; 
 } 
-
-// Main logic
-void heapify (int arr[], int n, int i) {
-    int largest = i;
-    int l = 2*i + 1;
-    int r = 2*i + 2;
-
-    if (l<n && arr[l]>arr[largest])
-        largest = l;
-    
-    if (r<n && arr[r]>arr[largest])
-        largest = r;
-
-    if (largest != i) {
-        swap (arr[i], arr[largest]);
-        heapify(arr, n, largest);
-    }
-}
-
-void heapSort (int arr[], int n) {
-    for (int i = n/2-1; i >= 0; i--)
-        heapify(arr, n, i);
-    for (int i = n-1; i>=0; i--) {
-        swap (arr[0], arr[i]);
-        heapify(arr, i, 0);
-    }
-}
-
-/* -------------- MAIN DRIVER CODE ----------------- */
+  
+// Driver program 
 int main() 
 { 
     int arr[] = {12, 11, 13, 5, 6, 7}; 
@@ -57,6 +60,6 @@ int main()
   
     heapSort(arr, n); 
   
-    cout << "Sorted array is: \n"; 
+    cout << "Sorted array is \n"; 
     printArray(arr, n); 
 } 
